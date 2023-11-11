7d44fd2005aa8d5b4878ce233607c6e1d6e54831
AnduinXue <anduin@aiursoft.com>
2023-11-11 13:06:16 +0000

    New test ping.

--- tests/Aiursoft.CSTools.Tests/Services/CommandServiceTests.cs
+++ tests/Aiursoft.CSTools.Tests/Services/CommandServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using Aiursoft.CSTools.Services;
+﻿using System.Runtime.InteropServices;
+using Aiursoft.CSTools.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Aiursoft.CSTools.Tests.Services;
@@ -9,10 +10,35 @@ public class CommandServiceTests
     [TestMethod]
     public async Task TestPing()
     {
-        var service = new CommandService();
-        var (code, output, error) = await service.RunCommandAsync("ping", "bing.com -n 1", Path.GetTempPath());
-        Assert.IsTrue(output.Contains("Pinging bing.com"));
-        Assert.IsTrue(string.IsNullOrEmpty(error));
-        Assert.AreEqual(0, code);
+        // If Windows, -n 1
+        // If Linux, -c 1
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            var service = new CommandService();
+            var (code, output, error) = await service.RunCommandAsync("ping", "bing.com -n 1", Path.GetTempPath());
+            Assert.IsTrue(output.Contains("Pinging bing.com"));
+            Assert.IsTrue(string.IsNullOrEmpty(error));
+            Assert.AreEqual(0, code);
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            var service = new CommandService();
+            var (code, output, error) = await service.RunCommandAsync("ping", "bing.com -c 1", Path.GetTempPath());
+            Assert.IsTrue(output.Contains("Pinging bing.com"));
+            Assert.IsTrue(string.IsNullOrEmpty(error));
+            Assert.AreEqual(0, code);
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            var service = new CommandService();
+            var (code, output, error) = await service.RunCommandAsync("ping", "bing.com -c 1", Path.GetTempPath());
+            Assert.IsTrue(output.Contains("Pinging bing.com"));
+            Assert.IsTrue(string.IsNullOrEmpty(error));
+            Assert.AreEqual(0, code);
+        }
+        else
+        {
+            Assert.Fail("Unknown OS!");
+        }
     }
 }
