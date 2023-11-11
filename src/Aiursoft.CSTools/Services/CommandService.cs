141cf181ef74915d5bae4ff41de2356cb1a116f7
AnduinXue <anduin@aiursoft.com>
2023-11-11 12:58:25 +0000

    Write new command service.

---
+++ src/Aiursoft.CSTools/Services/CommandService.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Aiursoft.Scanner.Abstractions;
+
+namespace Aiursoft.CSTools.Services;
+
+public class CommandService : ITransientDependency
+{
+    public async Task<(int code, string output, string error)> RunCommandAsync(string bin, string arg, string path, bool getOutput = true)
+    {
+        var p = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = bin,
+                Arguments = arg,
+                WorkingDirectory = path,
+                UseShellExecute = !getOutput,
+                RedirectStandardOutput = getOutput,
+                RedirectStandardError = getOutput,
+            }
+        };
+        p.Start();
+        await p.WaitForExitAsync();
+        if (getOutput)
+        {
+            var output = await p.StandardOutput.ReadToEndAsync();
+            var error = await p.StandardError.ReadToEndAsync();
+            return (p.ExitCode, output, error);
+        }
+        else
+        {
+            return (p.ExitCode, string.Empty, string.Empty);
+        }
+    }
+}
---
+++ tests/Aiursoft.CSTools.Tests/Services/CommandServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using Aiursoft.CSTools.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aiursoft.CSTools.Tests.Services;
+
+[TestClass]
+public class CommandServiceTests
+{
+    [TestMethod]
+    public async Task TestPing()
+    {
+        var service = new CommandService();
+        var (code, output, error) = await service.RunCommandAsync("ping", "bing.com -n 1", Path.GetTempPath());
+        Assert.IsTrue(output.Contains("Pinging bing.com"));
+        Assert.IsTrue(string.IsNullOrEmpty(error));
+        Assert.AreEqual(0, code);
+    }
+}
