ff461dd35289279d718b15e134a682b7744fd903
AnduinXue <anduin@aiursoft.com>
2024-02-02 08:31:14 +0000

    Update version and add ReflectionExtensions
    Updated version of Aiursoft.CSTools in project settings from 7.0.12 to 7.0.13. Added a new class 'ReflectionExtensions', providing extension methods for reflection-related operations. Corresponding test cases have been included under 'ReflectionExtensionsTests'.

---
+++ src/Aiursoft.CSTools/Services/ReflectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace Aiursoft.CSTools.Services;
+
+/// <summary>
+/// Provides extension methods for reflection-related operations.
+/// </summary>
+public static class ReflectionExtensions
+{
+    /// <summary>
+    /// Sets a _private_ Property Value from a given Object. Uses Reflection.
+    /// Throws a ArgumentOutOfRangeException if the Property is not found.
+    /// </summary>
+    /// <typeparam name="TObject">Type of the Object</typeparam>
+    /// <typeparam name="TValue">Type of the Value</typeparam>
+    /// <param name="obj">Object from where the Property Value is set</param>
+    /// <param name="propName">Property name as string.</param>
+    /// <param name="val">Value to set.</param>
+    /// <returns>PropertyValue</returns>
+    public static void SetPrivatePropertyValue<TObject, TValue>(this TObject obj, string propName, TValue val)
+        where TObject : class
+    {
+        var t = typeof(TObject);
+        var prop = t.GetProperty(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+        if (prop == null)
+        {
+            throw new ArgumentOutOfRangeException(nameof(propName),
+                $"Property {propName} was not found in Type {t.FullName}");
+        }
+
+        prop.SetValue(obj, val, null);
+    }
+}
---
+++ tests/Aiursoft.CSTools.Tests/Services/ReflectionExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using Aiursoft.CSTools.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aiursoft.CSTools.Tests.Services;
+
+[TestClass]
+public class ReflectionExtensionsTests
+{
+    [TestMethod]
+    public void SetPrivatePropertyValue_WhenCalled_ShouldSetPrivateProperty()
+    {
+        // Arrange
+        var obj = new TestClass();
+        const string propName = "PrivateProperty";
+        const int val = 10;
+
+        // Act
+        obj.SetPrivatePropertyValue(propName, val);
+
+        // Assert
+        Assert.AreEqual(val, obj.PrivateProperty);
+    }
+
+    private class TestClass
+    {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Local
+        public int PrivateProperty { get; private set; }
+    }
+}
